13c96bbb1b12be9396ce8d28227e21be9d81c583
IronLaddle <abdtasnim0@gmail.com>
2020-05-23 15:51:48 +0800

    update web config and add toilet controller

--- src/main/java/net/sendukbesi/springbootrest/WebConfig.java
+++ src/main/java/net/sendukbesi/springbootrest/WebConfig.java
@@ -6,10 +6,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
+//@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 	@Bean
     ServletRegistrationBean h2servletRegistration(){
@@ -22,5 +23,15 @@ public class WebConfig implements WebMvcConfigurer {
 	    public void addCorsMappings(CorsRegistry registry) {
 	        registry.addMapping("/**");
 	    }
+	 
+	 @Override
+	    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+	        registry.addResourceHandler("swagger-ui.html")
+	                .addResourceLocations("classpath:/META-INF/resources/");
+
+	        registry.addResourceHandler("/webjars/**")
+	                .addResourceLocations("classpath:/META-INF/resources/webjars/");    
+	   }
 
 }
--- src/main/java/net/sendukbesi/springbootrest/toilet/ControllerToilet.java
+++ src/main/java/net/sendukbesi/springbootrest/toilet/ControllerToilet.java
@@ -1,6 +1,5 @@
 package net.sendukbesi.springbootrest.toilet;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import net.sendukbesi.springbootrest.exception.ResourceNotFoundException;
 import net.sendukbesi.springbootrest.util.BaseController;
 import net.sendukbesi.springbootrest.util.BaseUtil;
@@ -37,23 +37,77 @@ public class ControllerToilet extends BaseController {
 	@Autowired
     private RepoToiletUser userToiletRepository;
 	
-	
+	@ApiOperation(value = "Get all Toilet")
 	@GetMapping("/alltoilet")
     public MessageResponse getAllToilet() {
     	List <ModelToilet> toilet = toiletRepository.findAll();
-    	if(!BaseUtil.isObjNull(toilet)) {
+    	if(toilet.size() > 0) {
     		return new MessageResponse(SendukErrorCodeEnum.SENDUK001, toilet);
     	} else {
-    		return new MessageResponse(SendukErrorCodeEnum.SENDUK005, null);
+    		//return new MessageResponse(SendukErrorCodeEnum.SENDUK005, null);
+    		
+    		ModelToilet toiletz = new ModelToilet();
+    		ModelToilet toilet1 = new ModelToilet();
+    		ModelToilet toilet2 = new ModelToilet();
+
+    		ModelToiletUser toiletUser = new ModelToiletUser();
+    		if (toiletRepository.findByName("Tandas Klia 1") == null) {
+    			System.err.println("masuk2");
+    			toiletz.setName("Tandas Klia 1");
+    			toiletz.setImage("http://www.hawkrentacar.com.my/wp-content/uploads/2016/06/OneRepublic-in-Malaysia-Arrive-KLIA-from-Singapore-7824_0.jpg");
+    			toiletz.setLocation("KLIA Airport");
+    			toiletz.setPrice(5);
+    			toiletz.setUsed(75);
+    			toiletz.setRating(4);
+    			toiletz.setDuration(1);
+    			toiletz.setDescription("Toilet saya ini telah menjadi toilet kegemaran warga KL yang inginkan bersendiririan. Mereka boleh melayari internet secara percuma");
+    			toiletz.setToiletOwnerID(0);
+    			toiletRepository.save(toiletz);
+
+    			toilet1.setName("Tandas Klia 2");
+    			toilet1.setImage("https://themangoroad.com/wp-content/uploads/2019/08/klia_2_01-1400x934.jpg");
+    			toilet1.setLocation("KLIA 2 Airport");
+    			toilet1.setPrice(10);
+    			toilet1.setUsed(175);
+    			toilet1.setRating(4);
+    			toilet1.setDuration(1);
+    			toilet1.setDescription("Toilet saya ini telah menjadi toilet kegemaran warga KLIA yang inginkan bersendiririan. Mereka boleh melayari internet secara percuma");
+    			toilet1.setToiletOwnerID(0);
+    			toiletRepository.save(toilet1);
+
+    			toilet2.setName("Tandas Klia 3");
+    			toilet2.setImage("https://themangoroad.com/wp-content/uploads/2019/08/klia_2_01-1400x934.jpg");
+    			toilet2.setLocation("KLIA 3 Airport");
+    			toilet2.setPrice(20);
+    			toilet2.setUsed(5);
+    			toilet2.setRating(2);
+    			toilet2.setDuration(1);
+    			toilet2.setDescription("Toilet saya ini telah menjadi toilet kegemaran warga KLIA yang inginkan bersendiririan. Mereka boleh melayari internet secara percuma");
+    			toilet2.setToiletOwnerID(0);
+    			toiletRepository.save(toilet2);
+
+    			toiletUser.setName("Taka");
+    			toiletUser.setImage("shorturl.at/uDHL8");
+    			toiletUser.setPassword("admin");
+    			toiletUser.setPhoneNumber(0111111);
+    			toiletUser.setEmail("admin@admin.com");
+    			toiletUser.setWallet(50);
+    			userToiletRepository.save(toiletUser);
+    		}
+    		List <ModelToilet> toiletzz = toiletRepository.findAll();
+    		return new MessageResponse(SendukErrorCodeEnum.SENDUK001, toiletzz);
     	}
     }
 	
+	@ApiOperation(value = "Get toilet by ID")
 	@GetMapping("/toilet/{id}")
     public MessageResponse getToiletById(@PathVariable("id") Long toiletId) {
         Optional<ModelToilet> toilet = toiletRepository.findById(toiletId);
         return new MessageResponse(SendukErrorCodeEnum.SENDUK001, toilet);
     }
-
+	
+	
+	@ApiOperation(value = "Create toilet")
     @PostMapping("/toilet")
     public MessageResponse createToilet(ModelToilet toilets) {
     	for (ModelToilet toilet : toiletRepository.findAll()) {
@@ -75,6 +129,8 @@ public class ControllerToilet extends BaseController {
         return new MessageResponse(SendukErrorCodeEnum.SENDUK001, "create toilet success");
     }
     
+	
+	@ApiOperation(value = "Update toilet")
     @PutMapping("/toilet/{id}")
     public MessageResponse updateToilet(@PathVariable(value = "id") Long toiletId,
         @Valid @RequestBody ModelToilet toiletDetails) throws ResourceNotFoundException {
@@ -93,7 +149,8 @@ public class ControllerToilet extends BaseController {
         final ModelToilet updatedToilet = toiletRepository.save(toilet);
         return new MessageResponse(SendukErrorCodeEnum.SENDUK001, "update success");
     }
-
+	
+	@ApiOperation(value = "Delete toilet")
     @DeleteMapping("/toilet/{id}")
     public MessageResponse deleteToilet(@PathVariable(value = "id") Long toiletId)
     throws ResourceNotFoundException {
